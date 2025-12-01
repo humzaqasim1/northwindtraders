fd57dc79aaa844558453df8042b127dc4a1c11bb
humzaqasim1 <humzaqasim1@gmail.com>
2025-12-01 14:17:10 -0600

    did exercise 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,8 +13,17 @@ public class Main {
         String choice = scanner.nextLine();
         switch (choice){
             case "1":
-
+                queryProducts();
+                break;
+            case "2":
+                queryCustomers();
+                break;
+            default:
+                System.out.println("Nah");
         }
+    }
+
+    public static void queryProducts() {
         try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");) {
             Statement statement = connection.createStatement();
             String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products ";
@@ -32,6 +41,33 @@ public class Main {
                         "Unit Price :  " + unitPrice + "\n" +
                         "Units In Stock :  " + unitsInStock
                         + "\n========================");
+            }
+
+        } catch (SQLException e){
+            System.err.println("Whoops");
+        }
+    }
+
+    public static void queryCustomers() {
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");) {
+            Statement statement = connection.createStatement();
+            String query = "SELECT ContactName, CompanyName, City, Country, Phone FROM Customers ";
+
+            ResultSet results = statement.executeQuery(query);
+
+            while (results.next()) {
+                String contactName = results.getString("ContactName");
+                String companyName = results.getString("CompanyName");
+                String city = results.getString("City");
+                String country = results.getString("Country");
+                String phone = results.getString("Phone");
+                System.out.println(
+                        "Contact Name :  " + contactName + "\n" +
+                                "Company Name :  " + companyName + "\n" +
+                                "City :  " + city + "\n" +
+                                "Country :  " + country + "\n" +
+                                "Phone :  " + phone +
+                                "\n========================");
             }
 
         } catch (SQLException e){
